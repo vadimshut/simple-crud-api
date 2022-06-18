262f456f4ba6203c0bf46af0b358b520929b3c17
Vadzim Shut <vadzim_shut@epam.com>
2022-06-18 12:16:28 +0300

    feat: implement delete function

--- src/router/delete.ts
+++ src/router/delete.ts
@@ -1,6 +1,26 @@
 import { IncomingMessage, ServerResponse } from "http";
+import { url } from "inspector";
+import { GET_USER_REGESP, INCORRECT_PATH } from "../constants";
 import { IUser } from "../model/IUser";
+import { getNameFromPath } from "../utils/get-name-from-path";
+import { deleteUser, findUser } from "../utils/user-operations";
+import { sendResponse } from "../utils/utils";
 
-export const deleteRequest = async (req: IncomingMessage, res: ServerResponse, GLOBAL_DATA: IUser[] | []) => {
-    console.log(req, res, GLOBAL_DATA);
+export const deleteRequest = async (req: IncomingMessage, res: ServerResponse, GLOBAL_DATA: IUser[]) => {
+    const {url} = req  
+
+    if(url && GET_USER_REGESP.test(url)) {
+        const userId = await getNameFromPath(url) 
+        const {statusCode, message, payload: foundUser} = await findUser(GLOBAL_DATA, userId, "DELETE")
+        if(statusCode >= 300) {
+            await sendResponse(res, statusCode, null, message)
+            return
+        }
+        if(foundUser?.id) {
+            await deleteUser(foundUser.id, GLOBAL_DATA)
+            sendResponse(res, statusCode, null, message)
+        }
+    }else {
+        await sendResponse(res, 404, null, INCORRECT_PATH)
+    }
 }
--- src/utils/user-operations.ts
+++ src/utils/user-operations.ts
@@ -2,15 +2,17 @@ import { v4 as uuidv4, validate } from 'uuid';
 import { MESSAGE_IS_NOT_UUID, NOT_FOUND } from '../constants';
 import { IUser } from '../model/IUser';
 
-const findUser = async (data: IUser[] | [], id: string) => {
-    const isValidated = validate(id)
+const findUser = async (data: IUser[] | [], id: string, method?: string) => {
+    const isValidated = validate(id) 
+    let statusCode = method === 'DELETE' ? 204 : 200
+
     if(!isValidated) {
         return {statusCode: 400, message: MESSAGE_IS_NOT_UUID}
     }
     const userList = data.filter(user => user.id === id)
 
     if(userList.length && userList[0]) {
-        return {statusCode: 200, message: '', payload: userList[0]}
+        return {statusCode, message: '', payload: userList[0]}
     }
     return {statusCode: 404, message: NOT_FOUND} 
 }
@@ -29,12 +31,22 @@ const updateUser = async (oldUser: IUser, newUser: IUser, GLOBAL_DATA: IUser[]) 
     const index = GLOBAL_DATA.findIndex(user => user.id === oldUser.id)
     const updatedUser: IUser = {id, ...newUser}
     GLOBAL_DATA.splice(index, 1, updatedUser)
+    console.log(`- User ${id} was update`);
+    
     return updatedUser   
+}
+
+const deleteUser = async (id: string, GLOBAL_DATA: IUser[]) => {
+    const index = GLOBAL_DATA.findIndex(user => user.id === id)
+    GLOBAL_DATA.splice(index, 1)
+    console.log(`- User ${id} was delete successfully`);
+    
 }
 
 
 export {
     findUser,
     createNewUser,
-    updateUser
+    updateUser,
+    deleteUser
 }
